71ff03ad523c82ea6a8e9617cc1d84c006e92e4a
phamvanhaid512 <132875905+phamvanhaid512@users.noreply.github.com>
2023-11-02 19:25:46 +0700

    jai

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,22 +8,22 @@ import { Pet } from './pet/entities/pet.entity';
 // import {dataSourceOptions} from './db/data-source.';
 import { PetModule } from './pet/pet.module';
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type:'mysql',
-      host: 'localhost',
-      port: 3306,
-      username:'root',
-      password:'456RTYfgh!@#',
-      database:'haipham',
-      entities:[User,Pet],
-      migrations: ['dist/db/migration/*.js'],
-      synchronize:true
-    }),
-    // TypeOrmModule.forRoot(dataSourceOptions),
-    UsersModule,
-    PetModule
-  ],
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type:'mysql',
+  //     host: 'localhost',
+  //     port: 3306,
+  //     username:'root',
+  //     password:'456RTYfgh!@#',
+  //     database:'haipham',
+  //     entities:[User,Pet],
+  //     migrations: ['dist/db/migration/*.js'],
+  //     synchronize:true
+  //   }),
+  //   // TypeOrmModule.forRoot(dataSourceOptions),
+  //   UsersModule,
+  //   PetModule
+  // ],
   
   controllers: [AppController],
   providers: [AppService],
